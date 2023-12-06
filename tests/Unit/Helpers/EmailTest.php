f5eb0d418166d2621e969c13d172349bd4b1eaf5
Noirifir <Augusto_L2K49@hotmail.com>
2023-12-06 12:06:33 -0300

    init config Email test

--- tests/Unit/Helpers/EmailTest.php
+++ tests/Unit/Helpers/EmailTest.php
@@ -13,4 +13,13 @@ class EmailTest extends TestCase
     {
         $this->assertTrue(true);
     }
+    public function testEmail(){
+        //esto no es correcto debe ser analizado por una clase externa
+        //$email='a@a@.com';
+        //$result=(bool) filter_var($email,FILTER_VALIDATE_EMAIL);
+
+        //como se debe realizar probando resultados de algo externo
+        $result =Email::validate('a@a.com');
+        $this->assertTrue($result);
+    }
 }
